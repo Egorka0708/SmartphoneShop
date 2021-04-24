790f244a2c8f86004e34dc9137d74713ec6fa992
dravonosec <dravonosec@yandex.ru>
2021-04-24 15:45:40 +0600

    Add alpha-version Session to project

--- shop/src/main/java/naumen/project/shop/ShopApplication.java
+++ shop/src/main/java/naumen/project/shop/ShopApplication.java
@@ -22,17 +22,18 @@ public class ShopApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(ShopApplication.class);
-		}
+	}
 
 	@Bean
-	CommandLineRunner runner(SmartphoneService smartphoneService){
+	CommandLineRunner runner(SmartphoneService smartphoneService) {
 		return args -> {
 			// read JSON and load json
 			// чтобы это работало, надо в pgAdmin на таблице ввести скрипт (правой кнопкой - скрипт)
 			// CREATE SEQUENCE hibernate_sequence START 1;
 			// id начнут сами проставляться. Похоже из за того что мы ручками делали бд, у нас по умолчанию это не стоит
 			ObjectMapper mapper = new ObjectMapper();
-			TypeReference<List<Smartphone>> typeReference = new TypeReference<List<Smartphone>>(){};
+			TypeReference<List<Smartphone>> typeReference = new TypeReference<List<Smartphone>>() {
+			};
 			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/smartphonesDB.json");
 			try {
 				List<Smartphone> smartphones = mapper.readValue(inputStream,typeReference);
@@ -40,11 +41,8 @@ public class ShopApplication {
 					if (!smartphones.contains(smartphone))
 						smartphoneService.save(smartphone);
 				}
-				System.out.println("Smartphones Saved!");
-			} catch (IOException e){
-				System.out.println("Unable to save smartphones: " + e.getMessage());
-			}
-		};
-	}
+			} ;
+		}
 
+	}
 }
--- shop/src/main/java/naumen/project/shop/controllers/SmartphonesController.java
+++ shop/src/main/java/naumen/project/shop/controllers/SmartphonesController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SmartphonesController {
+
     @Autowired
     private SmartphoneService smartphoneService;
 
